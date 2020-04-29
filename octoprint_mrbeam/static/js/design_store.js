5ae6226ede9b6b4367603db6b288454cd3ea7a87
Iratxe Espartza Chueca <iratxe@mr-beam.org>
2020-04-29 17:01:20 +0200

    Fix design store address

--- octoprint_mrbeam/static/js/design_store.js
+++ octoprint_mrbeam/static/js/design_store.js
@@ -3,7 +3,7 @@ $(function () {
         let self = this;
         window.mrbeam.viewModels['designStore'] = self;
 
-        self.DESIGN_STORE_IFRAME_SRC = 'http://localhost:8080';
+        self.DESIGN_STORE_IFRAME_SRC = 'https://design-store-269610.appspot.com';  // Don't write a "/" at the end!!
 
         self.loginState = params[0];
         self.navigation = params[1];
